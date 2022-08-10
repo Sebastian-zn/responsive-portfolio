07a0acdb5e340038c5a6832a1ad7d42179cf89e5
Sebastian <sebastian.aavn@outlook.com>
2022-08-09 23:19:17 -0500

    seccion 'Testimonial' terminada (y anteriores)

---
+++ js/main.js
@@ -0,0 +1,119 @@
+const nav_menu = document.getElementById('nav-menu'),
+  nav_toggle = document.getElementById('nav-toggle'),
+  nav_close = document.getElementById('nav-close');
+
+// Validate if constant exists
+if (nav_toggle) {
+  nav_toggle.addEventListener('click', () => {
+    nav_menu.classList.add('show-menu');
+  });
+}
+
+if (nav_close) {
+  nav_close.addEventListener('click', () => {
+    nav_menu.classList.remove('show-menu');
+  });
+}
+
+// Link action
+const nav_link = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+  nav_menu.classList.remove('show-menu');
+}
+
+nav_link.forEach(n => n.addEventListener('click', linkAction));
+
+// Accordion Skills
+const skills_content = document.getElementsByClassName('skills__content'),
+  skills_header = document.querySelectorAll('.skills__header');
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  for (let i = 0; i < skills_content.length; i++) {
+    skills_content[i].className = 'skills__content skills__close';
+  }
+
+  if (itemClass === 'skills__content skills__close') {
+    this.parentNode.className = 'skills__content skills__open';
+  }
+}
+
+skills_header.forEach((element) => {
+  element.addEventListener('click', toggleSkills);
+});
+
+// Qualification Tabs
+const tabs = document.querySelectorAll('[data-target]'),
+  tab_contents = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.target);
+
+    tab_contents.forEach(tab_content => {
+      tab_content.classList.remove('qualification__active');
+    });
+    target.classList.add('qualification__active');
+
+    tabs.forEach(tab => {
+      tab.classList.remove('qualification__active');
+    });
+    tab.classList.add('qualification__active');
+  });
+});
+
+// Services Modal
+const modal_views = document.querySelectorAll('.services__modal'),
+  modal_buttons = document.querySelectorAll('.services__button'),
+  modal_closes = document.querySelectorAll('.services__modal-close');
+
+let modal = function (modal_click) {
+  modal_views[modal_click].classList.add('active-modal');
+}
+
+modal_buttons.forEach((modal_btn, i) => {
+  modal_btn.addEventListener('click', () => {
+    modal(i);
+  });
+});
+
+modal_closes.forEach((modal_close) => {
+  modal_close.addEventListener('click', () => {
+    modal_views.forEach((modal_view) => {
+      modal_view.classList.remove('active-modal');
+    });
+  });
+});
+
+// Swiper Pagination
+let swiperPagination = new Swiper(".portfolio__container", {
+  cssMode: true,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// Swiper Testimonial
+let swiperTestimonial = new Swiper(".testimonial__container", {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 48,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  breakpoints: {
+    568: {
+      slidesPerView: 2,
+    }
+  }
+});
